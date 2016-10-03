8a2db4c0472fd2ec075d0de4e07972fb86b2eb74
Llandy Riveron Del Risco <llandy3d@gmail.com>
2016-10-03 23:11:42 +0200

    added the user profile/wallet creation on signal user created

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.conf import settings
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
 
 # TODO add billing information
@@ -23,3 +25,12 @@ class Wallet(models.Model):
 
     def __str__(self):
         return self.user.username + "'s wallet"
+
+
+# SIGNALS
+def complete_user_profile(sender, instance, **kwargs):
+    """After the user is created, create the UserProfile and Wallet"""
+    UserProfile.objects.create(user=instance)
+    Wallet.objects.create(user=instance)
+
+post_save.connect(complete_user_profile, sender=User)
