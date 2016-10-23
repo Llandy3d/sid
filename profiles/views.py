fed37c043e9ead2686069a2ec91973d28af91f02
Llandy Riveron Del Risco <llandy3d@gmail.com>
2016-10-23 21:19:52 +0200

    started creating the base.html page

--- profiles/views.py
+++ profiles/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def test_view(request):
-    return render(request, 'profiles/index.html')
+    # return render(request, 'profiles/index.html')
+    return render(request, 'base.html')
--- teams/models.py
+++ teams/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.conf import settings
 
 
 def team_logo_path(instance, filename):
